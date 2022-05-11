87ed4ad520874576c6895ee66bc4d737588edc0c
OksanaVovk <oksvovk85@gmail.com>
2022-05-12 02:16:31 +0300

    first step of task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,90 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const KEY_PIXABAY = `27331775-d4865903e456a7e108fc4ea1d`;
+const axios = require('axios').default;
+
+async function getCarts(name) {
+  const optionsSearch = `&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const url = `https://pixabay.com/api/?key=${KEY_PIXABAY}${optionsSearch}`;
+  console.log(url);
+  try {
+    const response = await axios.get(url);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+formEl.addEventListener('submit', onSearchPicture);
+function onSearchPicture(event) {
+  event.preventDefault();
+  clearGallery();
+  const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+  console.log(searchQuery);
+  getCarts(searchQuery).then(response => {
+    const picturesArray = response.data.hits;
+    console.log(picturesArray);
+    const markup_cards = createCards(picturesArray);
+    galleryEl.insertAdjacentHTML('beforeend', markup_cards);
+    const lightbox = new SimpleLightbox('.gallery a', {});
+  });
+}
+
+function createCards(data) {
+  return data
+    .map(({ likes, tags, views, downloads, comments, webformatURL, largeImageURL }) => {
+      return `<li class="photo-card">
+      <a class="gallery__item" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <a/>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</li>
+    `;
+    })
+    .join('');
+}
+
+function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+// function fetchCountries(name) {
+//   const optionsSearch = `&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`;
+//   const url = `https://pixabay.com/api/?key=${KEY_PIXABAY}${optionsSearch}`;
+//   return fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+// fetchCountries('cat')
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
